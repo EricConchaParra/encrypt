e8a57008d06e955a8c8b5da5aa6184b3bc1c1801
EricConchaParra <85537653+EricConchaParra@users.noreply.github.com>
2022-07-18 17:52:29 -0400

    CleanUp

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Encriptador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.encriptarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desencriptarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.txtDesEncriptado = new System.Windows.Forms.TextBox();
+            this.txtEncriptado = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEncriptar = new System.Windows.Forms.Button();
+            this.btnDesEncriptar = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem,
+            this.encriptarToolStripMenuItem,
+            this.desencriptarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(667, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // encriptarToolStripMenuItem
+            // 
+            this.encriptarToolStripMenuItem.Name = "encriptarToolStripMenuItem";
+            this.encriptarToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.encriptarToolStripMenuItem.Text = "Encriptar";
+            this.encriptarToolStripMenuItem.Click += new System.EventHandler(this.encriptarToolStripMenuItem_Click);
+            // 
+            // desencriptarToolStripMenuItem
+            // 
+            this.desencriptarToolStripMenuItem.Name = "desencriptarToolStripMenuItem";
+            this.desencriptarToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.desencriptarToolStripMenuItem.Text = "Desencriptar";
+            this.desencriptarToolStripMenuItem.Click += new System.EventHandler(this.desencriptarToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtClave);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(642, 47);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Clave para encriptación";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(6, 17);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.PasswordChar = '*';
+            this.txtClave.Size = new System.Drawing.Size(626, 20);
+            this.txtClave.TabIndex = 0;
+            this.txtClave.UseSystemPasswordChar = true;
+            // 
+            // txtDesEncriptado
+            // 
+            this.txtDesEncriptado.Location = new System.Drawing.Point(12, 116);
+            this.txtDesEncriptado.Multiline = true;
+            this.txtDesEncriptado.Name = "txtDesEncriptado";
+            this.txtDesEncriptado.Size = new System.Drawing.Size(288, 322);
+            this.txtDesEncriptado.TabIndex = 2;
+            // 
+            // txtEncriptado
+            // 
+            this.txtEncriptado.Location = new System.Drawing.Point(356, 116);
+            this.txtEncriptado.Multiline = true;
+            this.txtEncriptado.Name = "txtEncriptado";
+            this.txtEncriptado.Size = new System.Drawing.Size(288, 322);
+            this.txtEncriptado.TabIndex = 3;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(353, 95);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(79, 18);
+            this.label.TabIndex = 4;
+            this.label.Text = "Encriptado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Desencriptado";
+            // 
+            // btnEncriptar
+            // 
+            this.btnEncriptar.Location = new System.Drawing.Point(306, 173);
+            this.btnEncriptar.Name = "btnEncriptar";
+            this.btnEncriptar.Size = new System.Drawing.Size(44, 35);
+            this.btnEncriptar.TabIndex = 6;
+            this.btnEncriptar.Text = "-->";
+            this.btnEncriptar.UseVisualStyleBackColor = true;
+            this.btnEncriptar.Click += new System.EventHandler(this.btnEncriptar_Click);
+            // 
+            // btnDesEncriptar
+            // 
+            this.btnDesEncriptar.Location = new System.Drawing.Point(306, 214);
+            this.btnDesEncriptar.Name = "btnDesEncriptar";
+            this.btnDesEncriptar.Size = new System.Drawing.Size(44, 35);
+            this.btnDesEncriptar.TabIndex = 7;
+            this.btnDesEncriptar.Text = "<--";
+            this.btnDesEncriptar.UseVisualStyleBackColor = true;
+            this.btnDesEncriptar.Click += new System.EventHandler(this.btnDesEncriptar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 450);
+            this.Controls.Add(this.btnDesEncriptar);
+            this.Controls.Add(this.btnEncriptar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txtEncriptado);
+            this.Controls.Add(this.txtDesEncriptado);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Encriptador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem encriptarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desencriptarToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.TextBox txtDesEncriptado;
+        private System.Windows.Forms.TextBox txtEncriptado;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEncriptar;
+        private System.Windows.Forms.Button btnDesEncriptar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encriptador
+{
+    public partial class Form1 : Form
+    {
+         public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void goEncrypt()
+        {
+            txtEncriptado.Text = Encriptar(txtDesEncriptado.Text);
+        }
+
+        private void goDesEncrypt()
+        {
+            txtDesEncriptado.Text = DesEncriptar(txtEncriptado.Text);
+        }
+
+
+        public string Encriptar(string texto)
+        {
+            try
+            {
+
+                string key = txtClave.Text; //llave para encriptar datos
+
+                byte[] keyArray;
+
+                byte[] Arreglo_a_Cifrar = UTF8Encoding.UTF8.GetBytes(texto);
+
+                //Se utilizan las clases de encriptación MD5
+
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+
+                hashmd5.Clear();
+
+                //Algoritmo TripleDES
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+
+                tdes.Key = keyArray;
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = tdes.CreateEncryptor();
+
+                byte[] ArrayResultado = cTransform.TransformFinalBlock(Arreglo_a_Cifrar, 0, Arreglo_a_Cifrar.Length);
+
+                tdes.Clear();
+
+                //se regresa el resultado en forma de una cadena
+                texto = Convert.ToBase64String(ArrayResultado, 0, ArrayResultado.Length);
+
+            }
+            catch (Exception)
+            {
+
+            }
+            return texto;
+        }
+
+        public string DesEncriptar(string textoEncriptado)
+        {
+            try
+            {
+                string key = txtClave.Text;
+                byte[] keyArray;
+                byte[] Array_a_Descifrar = Convert.FromBase64String(textoEncriptado);
+
+                //algoritmo MD5
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+
+                hashmd5.Clear();
+
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+
+                tdes.Key = keyArray;
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = tdes.CreateDecryptor();
+
+                byte[] resultArray = cTransform.TransformFinalBlock(Array_a_Descifrar, 0, Array_a_Descifrar.Length);
+
+                tdes.Clear();
+                textoEncriptado = UTF8Encoding.UTF8.GetString(resultArray);
+
+            }
+            catch (Exception)
+            {
+
+            }
+            return textoEncriptado;
+        }
+
+        private void btnEncriptar_Click(object sender, EventArgs e)
+        {
+            goEncrypt();
+        }
+
+        private void btnDesEncriptar_Click(object sender, EventArgs e)
+        {
+            goDesEncrypt();
+        }
+
+        private void desencriptarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            goDesEncrypt();
+        }
+
+        private void encriptarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            goEncrypt();
+        }
+    }
+}
